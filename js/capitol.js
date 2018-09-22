dfe4483b8b539f8bb1d80d430532ac5c14074cc6
David Kerr <davallerr@gmail.com>
2018-09-21 23:02:29 -0700

    starts laying out capitol farms page

---
+++ js/capitol.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.getElementsByTagName('BODY');
+
+  var videos = [
+    '../../img/capitol-farms/pexels-video-4172.mp4',
+    '../../img/capitol-farms/pexels-video-4518.mp4',
+    '../../img/capitol-farms/pexels-video-1222739.mp4',
+    '../../img/capitol-farms/pexels-video-daisy.mp4',
+    '../../img/capitol-farms/pexels-video-purple.mp4'
+  ];
+
+  body.style.background = videos[i];
+});
